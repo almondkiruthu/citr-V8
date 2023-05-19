5af78e2139b594b7da645f53912903f578648868
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-05-19 13:50:17 +0300

    Created the Search param Component

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -2,14 +2,16 @@
 import React from "react";
 import "./App.css";
 import Pet from "./components/Pet.jsx";
+import SearchParams from "./SearchParams";
 
 function App() {
   return <>
     <div>
       <h1>Adopt me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <SearchParams />
+      {/* <Pet name="Luna" animal="dog" breed="Havanese" />
       <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <Pet name="Doink" animal="cat" breed="Mix" /> */}
     </div>
   </>;
 }
