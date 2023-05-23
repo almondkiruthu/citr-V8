62d5a3f885bbacfc057e4afd672bf563809485da
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-05-23 11:10:24 +0300

    Configured the react queery in my app

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import "./App.css";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Pet from "./components/Pet.jsx";
 import SearchParams from "./SearchParams";
 import Deatils from "./Deatils";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinty,
+      cacheTime: Infinty,
+    },
+  },
+});
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Deatils />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Deatils />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </QueryClientProvider>
       </BrowserRouter>
     </>
   );
