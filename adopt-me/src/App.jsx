b75faa6d9575f9d2ced54354fb15bea1e629f66d
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-05-23 12:07:28 +0300

    IMplemented the react query

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -4,7 +4,7 @@ import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Pet from "./components/Pet.jsx";
 import SearchParams from "./SearchParams";
-import Deatils from "./Deatils";
+import Details from "./Details";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,7 +24,7 @@ function App() {
             <Link to="/">Adopt Me!</Link>
           </header>
           <Routes>
-            <Route path="/details/:id" element={<Deatils />} />
+            <Route path="/details/:id" element={<Details />} />
             <Route path="/" element={<SearchParams />} />
           </Routes>
         </QueryClientProvider>
